a84318f8fa3f502f936ac1755419a7fd1c022d43
NoraGamesBr <noragamesbr@gmail.com>
2024-05-27 09:07:36 -0300

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,7 @@
+import random
+
+def generate_random_number():
+    return random randint(1, 500)
+
+random_number = generate_random_number()
+print (f"the random number you got is: {random_number}")
